e1bd03e1ce16361ed84574c866307b97c09e9759
SankalpYerigeri <160774479+SankalpYerigeri@users.noreply.github.com>
2024-02-24 16:36:43 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {}, "Hello World");
+const root = ReactDOM.getElementbyId("root");
+root.render(heading);
---
+++ README.md
@@ -0,0 +1,1 @@
+#I started learning React from here
