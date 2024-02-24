f9fb0ffc6212ef593df0ee735c7d9f411f0dc036
SankalpYerigeri <160774479+SankalpYerigeri@users.noreply.github.com>
2024-02-24 18:38:21 +0530

    conf files included

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
 const heading = React.createElement("h1", {}, "Hello World");
-const root = ReactDOM.getElementbyId("root");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
