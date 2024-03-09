138f8d7a3f9f75344e80a618b2c1ff64467383ee
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-10 00:24:05 +0530

    Created Functional component

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading = <h1>UVCE Prathibimba</h1>
+const Description = () =>
+{
+    return <>
+    <h4>Art and Photgraphy club of UVCE</h4>
+    </>
+}
+
+const HeadingComponent = () =>
+{
+    return <><h1>UVCE Prathibimba</h1>
+    <Description/>
+    </>
+    
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
