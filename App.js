37ae69f32fa6710cf261ad2153618cc9e8cef3b0
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-11 19:26:25 +0530

    Updated  Header, footer and Body

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import  ReactDOM  from "react-dom/client";
 
 const Header = () =>
 {
-    return <>
+    return( <>
     <div className="header">
     <div className="logo-container">
         <img className="logo" src="https://media.istockphoto.com/id/1184633031/vector/cartoon-burger-vector-isolated-illustration.jpg?s=612x612&w=0&k=20&c=Z66WFszea0EkDxLe2179qxjBi4zvsOVvQsZ3AbQRjB8="/>
@@ -18,33 +18,43 @@ const Header = () =>
     </div>
     </div>
     </>
+    );
 }
+
+const RestuarantCard = () =>
+{
+    return (
+        <div className="res-card">
+            <h3>Meghana Foods</h3>
+        </div>
+    )
+}
+
 
 const Body = () =>
 {
     return (
-        <>
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                //Res container
+                <RestuarantCard />
             </div>
 
         </div>
-        </>
-    )
-}
+    );
+};
 
 const AppLayout = () =>
 {
-    return <>
-    <Header/>
-    //Header
-    //Body
-    //Footer
+    return(
+
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+
+    );
     
-    </>
-    
-}
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout/>);
