41450331258d224690d93426f3c77c892756fc2f
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-08-05 23:21:41 +0530

    Implemented UseEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,33 @@
 import RestuarantCard from "./RestaurantCard";
 import { resList } from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () =>
     {
-        let [listofRes]= useState(resList);
+        let [listofRes, setlistofRes]= useState(resList);
+
+        useEffect(()=>
+        {
+            fetchData();
+        }, []);
+
+        const fetchData = async () =>
+        {
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+            const json = await data.json();
+
+            console.log(json);
+        }
+        
         return (
             <div className="body">
                 <div className="filter">
                     <button className="filter-btn"
                      onClick={()=> 
                      {
-                        listofRes = listofRes.filter((res)=>res.info.avgRating>4.3);
-                        console.log(listofRes);
+                        const fileterdList = listofRes.filter((res)=>res.info.avgRating>4.3);
+                        setlistofRes(fileterdList);
                      }
                      }>Top Rated Restuarants</button>
                 </div>
