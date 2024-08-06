db21e04b4295314491b1d26663edf85f29af0619
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-08-06 23:14:28 +0530

    Dynamic UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,12 +13,11 @@ const Body = () =>
 
         const fetchData = async () =>
         {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
 
             const json = await data.json();
 
-            console.log(json.data.cards[3].card.card.info);
-            setlistofRes(json.data.cards);
+            setlistofRes(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         }
         
         return (
