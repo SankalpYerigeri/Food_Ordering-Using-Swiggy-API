2a076e27596e5dc1f5ebbe45695134572dd6139e
sankalp <sankalpyerigeri20@gmail.com>
2024-08-30 16:58:30 +0530

    Class component learning

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,18 +4,56 @@ class UserClass extends React.Component
 {
     constructor(props)
     {
-        super(props);
+        super(props);  
+        
+        this.state =
+        {
+            userInfo : "Sankalp",
+            login : "Not me",
+            avatar_url : "dummy"
+        }
     }
+
+    async componentDidMount()
+    {
+        const data = await fetch("https://api.github.com/users/SankalpYerigeri");
+        const json = await data.json();
+
+        console.log(json);
+
+        this.setState({
+            userInfo : json
+        })
+    }
+
+    componentDidUpdate()
+    {
+        console.log("Component uodated")
+    }
+
+    componentWillUnmount()
+    {
+        console.log("Component unmounted")
+    }
+
+    
     render()
 
     {
-        const {name, location} = this.props;
+        const {location} = this.props;
+        const {name, login, avatar_url} = this.state.userInfo;
+        debugger
+
         return(
             <>
             <h1>{name}</h1>
-            <h2>{location}</h2>
+            <h2>{login}</h2>
+            <img className="w-20 h-20" src={avatar_url} alt="myphoto"/>
+            <h3>Contact : @sankalp_yerigeri</h3>
+            
             </>
         )
+
     }
 }
 
