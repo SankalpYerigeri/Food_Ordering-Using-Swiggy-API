1a411977baaf6fa4c9ba849b95688ad4cd4ae0c8
sankalp <sankalpyerigeri20@gmail.com>
2024-08-30 16:58:03 +0530

    Turned from functional comp to Class comp

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,33 @@
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () =>
+
+class About extends React.Component
 {
-    return(
-        <>
-        <h1>Hi this is About US</h1>
-        <UserClass name={"Sankalp Class"} location={"Gulbarga"}/>
-        </>
-    )
+    constructor(props)
+    {
+        super(props);
+        console.log("Parent constructor")
+    }
+
+    componentDidMount()
+    {
+        console.log("Parent Mounted")
+    }
+
+
+    render()
+    {
+        console.log("Parent render");
+        return(
+            <>
+            <h1>Hi this is About US</h1>
+            <UserClass name={"Sankalp Class"} location={"Gulbarga"}/>
+            </>
+        )
+    }
 }
+
+
 
 export default About;
